2142a8d0080785328dc684dbcd3e67e94bcb7891
Hyunjun Kim <hyunjun.kim133@gmail.com>
2024-02-15 18:09:20 +0900

    third commit

--- main.js
+++ main.js
@@ -11,53 +11,82 @@
 
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
+let tabs = document.querySelectorAll(".task-tabs div");
+let underLine = document.getElementById("under-line");
+
+let mode = "all";
 let taskList = [];
+let filterList = [];
 
 
 addButton.addEventListener("click", addTask);
+taskInput.addEventListener("keyup", (event) => {
+    if (event.key === 'Enter') {
+        addTask();
+        render();
+    }
+})
 addButton.addEventListener("click", render);
 taskInput.addEventListener("focus", () => {taskInput.value = ""}, );
 
 
-function addTask() {
-    let task = {
-        id : randomIdGenerate(),
-        taskContent : taskInput.value,
-        isComplete : false,
-    };
 
-    if (task.taskContent === "") {
-        return
+for (let i = 1; i < tabs.length; i++) {
+    tabs[i].addEventListener("click", function(event) {
+        filter(event)
+    })
+};
+
+
+function addTask() {
+    if (taskInput.value.length < 0 || taskInput.value.trim() === ""){
+        return alert("내용을 입력해주세요");
+    }
+    else {
+        let task = {
+            id : randomIdGenerate(),
+            taskContent : taskInput.value,
+            isComplete : false,
+        }
+        taskList.push(task)
     };
-    
-    taskList.push(task)
+    render();
     console.log(taskList)
 };
 
 
 function render() {
-    let resultHtml = "";
-    for (let i = 0; i < taskList.length; i++) {
+    let list = [];
 
-        if (taskList[i].isComplete === true) {
+    if (mode === "all") {
+        list = taskList;
+    }
+    else if (mode === "ongoing" || mode == "done") {
+        list = filterList;
+    }
+
+    let resultHtml = "";
+    for (let i = 0; i < list.length; i++) {
+
+        if (list[i].isComplete === true) {
             resultHtml += `<div class="task">
                 <div class="task-done">
-                    <span>${taskList[i].taskContent}</span>
+                    <span>${list[i].taskContent}</span>
                 </div>
                 <div class="button-area">
-                    <button onclick="toggleComplete('${taskList[i].id}')"><i class="fa-solid fa-rotate-right"></i></button>
-                    <button onclick="deleteTask('${taskList[i].id}')"><i class="fa-solid fa-trash"></i></button>
+                    <button onclick="toggleComplete('${list[i].id}')"><i class="fa-solid fa-rotate-right"></i></button>
+                    <button onclick="deleteTask('${list[i].id}')"><i class="fa-solid fa-trash"></i></button>
                 </div>
             </div>`
         }
         else {
             resultHtml += `<div class="task">
                 <div>
-                <span>${taskList[i].taskContent}</span>
+                <span>${list[i].taskContent}</span>
                 </div>
                 <div class="button-area">
-                    <button onclick="toggleComplete('${taskList[i].id}')"><i class="fa-solid fa-check"></i></button>
-                    <button onclick="deleteTask('${taskList[i].id}')"><i class="fa-solid fa-trash"></i></button>
+                    <button onclick="toggleComplete('${list[i].id}')"><i class="fa-solid fa-check"></i></button>
+                    <button onclick="deleteTask('${list[i].id}')"><i class="fa-solid fa-trash"></i></button>
                 </div>
             </div>`
         };
@@ -73,7 +102,7 @@ function toggleComplete(id) {
             taskList[i].isComplete = !taskList[i].isComplete
             break
         };
-    }
+    };
     render();
     console.log(taskList);
 };
@@ -86,9 +115,44 @@ function deleteTask(id) {
             break
         };
     }
-    render();
+    filter();
     console.log(taskList);
 };
+
+
+function filter(event) {
+    filterList = [];
+
+    if (event) {
+        mode = event.target.id;
+        underLine.style.left = event.currentTarget.offsetLeft + "px";
+        underLine.style.width = event.currentTarget.offsetWidth + "px";
+        underLine.style.top = event.currentTarget.offsetTop + event.currentTarget.offsetHeight -2 + "px";
+    };
+
+
+    if (mode === "all") {
+        render()
+    }
+    else if (mode === "ongoing") {
+        for (let i = 0; i < taskList.length; i++) {
+            if (taskList[i].isComplete === false) {
+                filterList.push(taskList[i])
+            }
+        }
+        console.log(filterList);
+    }
+    else if (mode === "done") {
+        for (let i = 0; i < taskList.length; i++) {
+            if (taskList[i].isComplete === true) {
+                filterList.push(taskList[i])
+            }
+        }
+        console.log(filterList);
+    };
+    render();
+};
+
 
 
 function randomIdGenerate() {
